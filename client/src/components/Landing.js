b21c44ff60887e2667b21f22f8b591d928bbe4f9
Ryan Doherty <rjd.atx@gmail.com>
2020-04-20 21:14:09 -0500

    Header content and redirects

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Haven't heard back?</h1>
+      Collect feedback from your users instantly, via email!
+    </div>
+  );
+};
+
+export default Landing;
