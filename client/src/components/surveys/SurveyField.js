e1f37bf57f2a3e84df7efa674fc30ea187ed52a2
Ryan Doherty <rjd.atx@gmail.com>
2020-04-23 12:50:53 -0500

    SurveyField component creation

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+// SurveyField contains logic to render a single lable and text input
+
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
