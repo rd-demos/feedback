58ef2c1bab5342af848ac60aa33acedcbf7b73ca
Ryan Doherty <rjd.atx@gmail.com>
2020-04-24 13:41:31 -0500

    Email testing and validation for domains and etc.

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   {
@@ -65,6 +66,9 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
   _.each(FIELDS, ({ name, noValueError }) => {
     if (!values[name]) {
       errors[name] = noValueError;
